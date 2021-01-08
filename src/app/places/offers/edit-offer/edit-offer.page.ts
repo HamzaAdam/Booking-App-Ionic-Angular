d8845876d5352834e88593832faedf352767b3cb
Hamza Adam <hamza04060406@gmail.com>
2021-01-08 17:53:57 +0500

    place extracted by using activated route from param id

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { Place } from "../../place.model";
+import { PlacesService } from "../../places.service";
 
 @Component({
-  selector: 'app-edit-offer',
-  templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss'],
+  selector: "app-edit-offer",
+  templateUrl: "./edit-offer.page.html",
+  styleUrls: ["./edit-offer.page.scss"],
 })
 export class EditOfferPage implements OnInit {
+  place: Place;
 
-  constructor() { }
+  constructor(
+    private placeService: PlacesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((param) => {
+      this.place = this.placeService.getplace(param.get("placeId"));
+    });
   }
-
 }

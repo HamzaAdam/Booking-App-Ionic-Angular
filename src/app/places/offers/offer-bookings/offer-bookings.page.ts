4eb42f62d67df6c0f7893e666e73fda72ee24a04
Hamza Adam <hamza04060406@gmail.com>
2021-01-08 16:53:05 +0500

    Offer booking page to book an offer

---
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { Place } from "../../place.model";
+import { PlacesService } from "../../places.service";
+
+@Component({
+  selector: "app-offer-bookings",
+  templateUrl: "./offer-bookings.page.html",
+  styleUrls: ["./offer-bookings.page.scss"],
+})
+export class OfferBookingsPage implements OnInit {
+  place: Place;
+
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placesService: PlacesService
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((param) => {
+      if (!param.has("placeId")) {
+        this.navCtrl.navigateBack(["/", "places", "tabs", "offers"]);
+        return;
+      }
+      this.place = this.placesService.getplace(param.get("placeId"));
+    });
+  }
+}

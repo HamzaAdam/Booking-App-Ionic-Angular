1f0b3de296b908553f3060899fb9099e3ae77371
Hamza Adam <hamza04060406@gmail.com>
2021-01-08 20:34:43 +0500

    Some dummy authentication added

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./auth.service";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+  selector: "app-auth",
+  templateUrl: "./auth.page.html",
+  styleUrls: ["./auth.page.scss"],
 })
 export class AuthPage implements OnInit {
+  constructor(private authSer: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLogin() {
+    this.authSer.login();
+    console.log(this.authSer.isUserAuthenticated);
   }
-
 }

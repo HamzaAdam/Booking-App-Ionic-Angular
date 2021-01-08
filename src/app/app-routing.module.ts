b0ca9276a20ae8ce125d205c5819fd5d536a0f97
Hamza Adam <hamza04060406@gmail.com>
2021-01-08 16:45:39 +0500

    App-routing module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "",
+    redirectTo: "places",
+    pathMatch: "full",
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "auth",
+    loadChildren: () =>
+      import("./auth/auth.module").then((m) => m.AuthPageModule),
+  },
+  {
+    path: "places",
+    loadChildren: () =>
+      import("./places/places.module").then((m) => m.PlacesPageModule),
+  },
+  {
+    path: "bookings",
+    loadChildren: () =>
+      import("./bookings/bookings.module").then((m) => m.BookingsPageModule),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

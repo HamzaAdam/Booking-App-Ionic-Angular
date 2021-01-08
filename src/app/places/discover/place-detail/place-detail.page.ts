b334d9d434b298036adb83c18cbb17994fc92df5
Hamza Adam <hamza04060406@gmail.com>
2021-01-08 17:53:05 +0500

    Place extracted with the help of params using activated route

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { NavController } from "@ionic/angular";
+import { Place } from "../../place.model";
+import { PlacesService } from "../../places.service";
 
 @Component({
   selector: "app-place-detail",
@@ -8,9 +10,18 @@ import { NavController } from "@ionic/angular";
   styleUrls: ["./place-detail.page.scss"],
 })
 export class PlaceDetailPage implements OnInit {
-  constructor(private router: Router, private navCtrl: NavController) {}
+  place: Place;
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placeService: PlacesService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe((param) => {
+      this.place = this.placeService.getplace(param.get("placeId"));
+    });
+  }
 
   onPlaceBook() {
     // this.router.navigate(["places", "tabs", "discover"]);

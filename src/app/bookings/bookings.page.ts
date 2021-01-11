679fdaab222a9e015a9bd8d87b5f949a5d1019a8
Hamza Adam <hamza04060406@gmail.com>
2021-01-11 19:18:28 +0500

    booking service injected

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IonItemSliding } from "@ionic/angular";
+import { Booking } from "./booking.model";
+import { BookingService } from "./booking.service";
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.page.html',
-  styleUrls: ['./bookings.page.scss'],
+  selector: "app-bookings",
+  templateUrl: "./bookings.page.html",
+  styleUrls: ["./bookings.page.scss"],
 })
 export class BookingsPage implements OnInit {
+  loadedBookings: Booking[];
 
-  constructor() { }
+  constructor(private BookingService: BookingService) {}
 
   ngOnInit() {
+    this.loadedBookings = this.BookingService.bookings;
   }
 
+  onCancelBooking(bookingId: string, itemSliding: IonItemSliding) {
+    itemSliding.close();
+    console.log(bookingId);
+  }
 }

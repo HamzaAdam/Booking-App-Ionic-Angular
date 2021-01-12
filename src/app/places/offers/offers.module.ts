b26d6757319c8c8f5e8725523e73175eef0220a9
Hamza Adam <hamza04060406@gmail.com>
2021-01-12 18:05:14 +0500

    offer-item component created
    component created for more styling

--- src/app/places/offers/offers.module.ts
+++ src/app/places/offers/offers.module.ts
@@ -1,20 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { OffersPageRoutingModule } from './offers-routing.module';
+import { OffersPageRoutingModule } from "./offers-routing.module";
 
-import { OffersPage } from './offers.page';
+import { OffersPage } from "./offers.page";
+import { OfferItemComponent } from "./offer-item/offer-item.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    OffersPageRoutingModule
-  ],
-  declarations: [OffersPage]
+  imports: [CommonModule, FormsModule, IonicModule, OffersPageRoutingModule],
+  declarations: [OffersPage, OfferItemComponent],
 })
 export class OffersPageModule {}

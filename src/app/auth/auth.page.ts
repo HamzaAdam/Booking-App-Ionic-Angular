f5275a3f5c4e96ec09defdf15c26f91979f29742
Hamza Adam <hamza04060406@gmail.com>
2021-01-12 16:04:55 +0500

    ion spinner and loadingController implemented

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { LoadingController } from "@ionic/angular";
 import { AuthService } from "./auth.service";
 
 @Component({
@@ -8,13 +9,29 @@ import { AuthService } from "./auth.service";
   styleUrls: ["./auth.page.scss"],
 })
 export class AuthPage implements OnInit {
-  constructor(private authSer: AuthService, private router: Router) {}
+  isLoading = false;
+
+  constructor(
+    private authSer: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {}
 
   onLogin() {
+    this.isLoading = true;
     this.authSer.login();
-    this.router.navigateByUrl("/places/tabs/discover");
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: "Logging In...." })
+      .then((loadingEl) => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl("/places/tabs/discover");
+        }, 2000);
+      });
     console.log(this.authSer.isUserAuthenticated);
   }
 }

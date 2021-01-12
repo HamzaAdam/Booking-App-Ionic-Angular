976986930a5e8c95b96b600f6c7d81181ce8c1a4
Hamza Adam <hamza04060406@gmail.com>
2021-01-12 16:02:58 +0500

    virtual scrolling and segmented button implemented
    virtual scrolling iplemented on our list

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { MenuController } from "@ionic/angular";
 import { Place } from "../place.model";
 import { PlacesService } from "../places.service";
 
@@ -9,11 +10,24 @@ import { PlacesService } from "../places.service";
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
-  constructor(private placesService: PlacesService) {}
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
+  ) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
     console.log(this.loadedPlaces);
+  }
+
+  segmentChanged(event) {
+    console.log(event.detail);
+  }
+
+  onOpenMenu() {
+    this.menuCtrl.toggle("first");
   }
 }

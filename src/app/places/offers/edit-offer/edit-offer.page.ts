1b89a6d0c57ba81d1613bca0146017ab30b28b36
Hamza Adam <hamza04060406@gmail.com>
2021-01-13 19:02:02 +0500

    reactive form created for editing offer

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { NavController } from "@ionic/angular";
 import { Place } from "../../place.model";
@@ -11,15 +12,37 @@ import { PlacesService } from "../../places.service";
 })
 export class EditOfferPage implements OnInit {
   place: Place;
+  form: FormGroup;
 
   constructor(
     private placeService: PlacesService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private navCtrl: NavController
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((param) => {
+      if (!param.has("placeId")) {
+        this.navCtrl.navigateBack("/places/tabs/offers");
+        return;
+      }
       this.place = this.placeService.getplace(param.get("placeId"));
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: "blur",
+          validators: [Validators.required],
+        }),
+        description: new FormControl(this.place.description, {
+          validators: [Validators.required, Validators.maxLength(150)],
+        }),
+      });
     });
+  }
+
+  onUpdateOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+    console.log(this.form);
   }
 }

b9513baf1a377c3a3d5444792cf1edce5e95b11c
Hamza Adam <hamza04060406@gmail.com>
2021-01-13 18:50:01 +0500

    Reactive form created for adding new offer

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-new-offer',
-  templateUrl: './new-offer.page.html',
-  styleUrls: ['./new-offer.page.scss'],
+  selector: "app-new-offer",
+  templateUrl: "./new-offer.page.html",
+  styleUrls: ["./new-offer.page.scss"],
 })
 export class NewOfferPage implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required],
+      }),
+      description: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required, Validators.maxLength(150)],
+      }),
+      price: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required, Validators.min(1)],
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required],
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: "blur",
+        validators: [Validators.required],
+      }),
+    });
   }
 
+  onSubmitOffer() {
+    console.log(this.form);
+  }
 }

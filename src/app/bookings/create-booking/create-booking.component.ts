feecf189f6613758a22262509bf458ec81c4c90b
Hamza Adam <hamza04060406@gmail.com>
2021-01-14 12:24:18 +0500

    Booking form added with date Controls
    Random date selection functions created with help of Math.random()

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -9,10 +9,40 @@ import { Place } from "src/app/places/place.model";
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() selectedMode: "select" | "random";
+  startDate: string;
+  endDate: string;
 
   constructor(private modalCtrl: ModalController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+    if (this.selectedMode === "random") {
+      this.startDate = new Date(
+        Math.floor(
+          Math.random() *
+            (availableTo.getTime() -
+              7 * 24 * 60 * 60 * 1000 -
+              availableFrom.getTime())
+        ) + availableFrom.getTime()
+      ).toISOString();
+      this.endDate = new Date(
+        Math.floor(
+          Math.random() *
+            (availableTo.getTime() - new Date(this.startDate).getTime()) +
+            new Date(this.startDate).getTime()
+        )
+      ).toISOString();
+      // new Date(
+      //   Math.floor(
+      //     Math.random() * (availableTo.getTime() - availableFrom.getTime()) +
+      //       availableFrom.getTime() +
+      //       new Date(this.startDate).getTime()
+      //   )
+      // ).toISOString();
+    }
+  }
 
   onPlaceBook() {
     this.modalCtrl.dismiss(

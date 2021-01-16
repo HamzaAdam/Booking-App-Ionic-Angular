a90dc2e0a64f814dc920574e02364253e1114b1a
Hamza Adam <hamza04060406@gmail.com>
2021-01-16 12:20:11 +0500

    booking form created and implemented

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
-import { IonItemSliding } from "@ionic/angular";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { IonItemSliding, LoadingController } from "@ionic/angular";
+import { Subscription } from "rxjs";
 import { Booking } from "./booking.model";
 import { BookingService } from "./booking.service";
 
@@ -8,17 +9,41 @@ import { BookingService } from "./booking.service";
   templateUrl: "./bookings.page.html",
   styleUrls: ["./bookings.page.scss"],
 })
-export class BookingsPage implements OnInit {
+export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
+  private bookingSubscription: Subscription;
 
-  constructor(private BookingService: BookingService) {}
+  constructor(
+    private BookingService: BookingService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
-    this.loadedBookings = this.BookingService.bookings;
+    this.bookingSubscription = this.BookingService.bookings.subscribe(
+      (bookings) => {
+        this.loadedBookings = bookings;
+      }
+    );
   }
 
   onCancelBooking(bookingId: string, itemSliding: IonItemSliding) {
     itemSliding.close();
+    this.loadingCtrl
+      .create({
+        message: "deleting your booking...",
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        this.BookingService.cancelBooking(bookingId).subscribe(() => {
+          loadingEl.dismiss();
+        });
+      });
     console.log(bookingId);
+  }
+
+  ngOnDestroy() {
+    if (this.bookingSubscription) {
+      this.bookingSubscription.unsubscribe();
+    }
   }
 }

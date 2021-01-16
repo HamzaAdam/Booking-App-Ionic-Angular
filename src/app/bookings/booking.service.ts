8b1a5f8837ace059d1c0541e292294401dc91e03
Hamza Adam <hamza04060406@gmail.com>
2021-01-16 12:18:05 +0500

    addbooking(),cancelBooking() methods created
    using RXJS behaviour subject

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,23 +1,60 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { delay, take, tap } from "rxjs/operators";
+
+import { AuthService } from "../auth/auth.service";
 import { Booking } from "./booking.model";
 
 @Injectable({
   providedIn: "root",
 })
 export class BookingService {
-  private _bookings: Booking[] = [
-    {
-      id: "xyz",
-      placeId: "p1",
-      userId: "abc",
-      placeTitle: "Karachi Mega City",
-      guestNumber: 50,
-    },
-  ];
+  private _bookings = new BehaviorSubject<Booking[]>([]);
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   get bookings() {
-    return [...this._bookings];
+    return this._bookings.asObservable();
+  }
+
+  addBooking(
+    placeId: string,
+    placeTitle: string,
+    placeImage: string,
+    firstName: string,
+    lastName: string,
+    guestNumber: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    const newBooking = new Booking(
+      Math.random().toString(),
+      placeId,
+      this.authService.userId,
+      placeTitle,
+      placeImage,
+      firstName,
+      lastName,
+      guestNumber,
+      dateFrom,
+      dateTo
+    );
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap((bookings) => {
+        this._bookings.next(bookings.concat(newBooking));
+      })
+    );
+  }
+
+  cancelBooking(id: string) {
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap((bookings) => {
+        this._bookings.next(bookings.filter((bookings) => bookings.id !== id));
+      })
+    );
   }
 }
